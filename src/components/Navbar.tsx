45c914bda63578c6afe58a2312c67d709d34f94d
AaaayushXD <aayushlamichhane.hehe@gmail.com>
2024-01-19 18:53:50 +0545

    navbar done

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,15 @@
-import { ArrowRight, Home, PhoneCall, Settings } from "lucide-react";
+import {
+  ArrowRight,
+  BarChart3,
+  CalendarDays,
+  Home,
+  PhoneCall,
+  Settings,
+} from "lucide-react";
 import { motion } from "framer-motion";
 import { useState } from "react";
+
+//TODO: Logo needed. this is NOT the actual LOGO.
 import Logo from "../assets/aayushlogo.png";
 
 const variants = {
@@ -13,6 +22,16 @@ const navItems = [
     name: "Home",
     url: "#",
     icon: <Home />,
+  },
+  {
+    name: "Date",
+    url: "/date",
+    icon: <CalendarDays />,
+  },
+  {
+    name: "Rent",
+    url: "/rent",
+    icon: <BarChart3 />,
   },
   {
     name: "Contacts",
@@ -33,7 +52,7 @@ export const Navbar: React.FC = () => {
         className="h-[100dvh] w-[20%] max-w-[250px] bg-[var(--primary-color)] px-5 py-8 relative min-w-[100px] transition-all duration-500 ease-in-out "
         style={{ zIndex: 10 }}
       >
-        <div className="flex flex-col items-center justify-between w-full h-full ">
+        <div className="flex flex-col items-center justify-between w-full h-full gap-10">
           <div className=" bg-[var(--primary-content)] rounded-full p-3">
             <img src={Logo} alt="Logo" className="w-[80px] h-[80px] p-4" />
           </div>
@@ -51,14 +70,18 @@ export const Navbar: React.FC = () => {
                   onClick={() => setActiveNavIndex(index)}
                 >
                   <a
+                    href={item.url}
                     className={
-                      "flex w-full gap-2 p-2 items-center transition-all duration-300 ease-in-out " +
+                      "flex w-full space-x-5 p-2 items-center transition-all duration-300 ease-in-out lg:justify-start " +
                       (isOpen ? " " : " justify-center")
                     }
                   >
                     <p>{item.icon}</p>
                     <p
-                      className={" md:block " + (isOpen ? " block" : " hidden")}
+                      className={
+                        "tracking-wide lg:block " +
+                        (isOpen ? " block" : " hidden")
+                      }
                     >
                       {item.name}
                     </p>
@@ -67,12 +90,12 @@ export const Navbar: React.FC = () => {
               );
             })}
           </div>
-          <div>
+          <div className="cursor-pointer text-[var(--copy)] hover:text-[var(--primary-content)] transition-all duration-500 ease-in-out">
             <Settings size={40} />
           </div>
         </div>
         <div
-          className="absolute top-[30px] right-[-15px] bg-[var(--secondary-content)] rounded-full p-2 cursor-pointer md:hidden transition-all ease-in-out duration-300 delay-300"
+          className="absolute top-[30px] right-[-15px] bg-[var(--secondary-content)] rounded-full p-2 cursor-pointer lg:hidden transition-all ease-in-out duration-300 delay-300"
           onClick={() => setIsOpen((isOpen) => !isOpen)}
         >
           <ArrowRight />
